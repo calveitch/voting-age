7a843bfe803c7cdc300bb583e7293e5961b72315
calveitch <159745564+calveitch@users.noreply.github.com>
2024-02-18 18:42:53 -0800

    Update voting-age.js

--- voting-age.js
+++ voting-age.js
@@ -1,15 +1,9 @@
 // Checks the given age to see whether it's legal to vote in the US or not.
 // Returns true if the age is 18 years or older. Otherwise, returns false.
 
-var age=18;
   // TODO: Implement this function.
 function checkVotingAge(age) {
-  if (age >= 18) 
-     const result = true;
-    } else {
-  const result = false;
-    }
-  return result;
+  return age >= 18 ;
 }
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
